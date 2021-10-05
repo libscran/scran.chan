2db03913cb748cbeb5a4697f8c8ca83f83a0714c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-10-04 20:38:48 -0700

    Added documentation for the all-neighbors function.

--- R/runAllNeighbors.R
+++ R/runAllNeighbors.R
@@ -1,3 +1,28 @@
+#' Run all neighbor-related steps
+#'
+#' Run all steps in the basic workflow that require nearest neighbor detection.
+#' 
+#' @param x Numeric matrix containing dimensions in the rows and cells in the columns.
+#' This is typically a matrix of principal components.
+#' @param tsne.perplexity Parameters to be used for t-SNE, see \code{\link{runTSNE.chan}} for details.
+#' @param umap.num.neighbors Parameters to be used for UMAP, see \code{\link{runUMAP.chan}} for details.
+#' @param snn.num.neighbors,snn.resolution Parameters to be used for graph-based clustering, see \code{\link{clusterSNNGraph.chan}} for details.
+#' @param num.threads Integer scalar specifying the number of threads to use.
+#' 
+#' @return A list containing \code{"snn"}, \code{"umap"} and \code{"tsne"},
+#' each of which contains the result of their respective function \code{*.chan} functions.
+#'
+#' @details
+#' By running all of these steps together, we can avoid redundant construction of the nearest neighbor index.
+#' We can also execute some of the single-threaded steps concurrently for further time savings.
+#'
+#' It is tempting to re-use the nearest neighbor search results across the different steps,
+#' subsetting to the number of neighbors required in each step.
+#' However, we do not do so as the approximate nature of the search means that the subsetting may not yield the same results as a direct search for the requested number of neighbors.
+#' This can lead to inconsistent behavior where the results of one step depend on whether other steps were run.
+#'
+#' @author Aaron Lun
+#' 
 #' @examples
 #' x <- t(as.matrix(iris[,1:4]))
 #' res <- runAllNeighbors(x)
