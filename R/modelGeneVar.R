c558464f37eee915a085658c7b7d457e8adf8484
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2023-04-21 09:28:50 -0700

    Propagate more smoother options during mean-var trend fitting. (#10)
    This introduces a "fixed width" mode to modelGeneVar.chan (from the
    corresponding options in the libscran library), which should improve the
    responsiveness of the fitted trend at high abundances.

--- R/modelGeneVar.R
+++ R/modelGeneVar.R
@@ -4,6 +4,11 @@
 #'
 #' @param x A list containing the log-expression matrix, typically produced by \code{\link{logNormCounts.chan}}.
 #' @param span Numeric scalar containing the span to use for LOWESS smoothing of the trend.
+#' @param use.fixed.width Logical scalar specifying whether to apply a fixed-width constraint on the smoothing,
+#' to avoid problems with large differences in density across the range of means.
+#' @param fixed.width Numeric scalar specifying the window width when `use.fixed.width = TRUE`.
+#' @param fixed.min.count Integer scalar specifying the minimum number of genes in a window when `use.fixed.width = TRUE`.
+#' Windows with too few genes are expanded until they contain the required minimum. 
 #' @param num.threads Integer scalar specifying the number of threads to use.
 #' @param batch Vector or factor of length equal to the number of cells, specifying the batch of origin for each cell.
 #' Alternatively \code{NULL} if all cells belong to the same batch.
@@ -27,9 +32,17 @@
 #' points(df$means, df$fitted, col="red", pch=16, cex=0.5)
 #' 
 #' @export
-modelGeneVar.chan <- function(x, span = 0.3, num.threads = 1, batch=NULL) {
+modelGeneVar.chan <- function(x, span = 0.3, use.fixed.width=FALSE, fixed.width=1, fixed.min.count=200, num.threads = 1, batch=NULL) {
     batch <- transform_factor(batch, n = tatami_ncol(x))
-    output <- model_gene_var(x$pointer, span, nthreads=num.threads, batch=batch$index)
+    output <- model_gene_var(
+        x$pointer, 
+        span=span, 
+        use_fixed=use.fixed.width, 
+        fixed_width=fixed.width, 
+        min_count=fixed.min.count, 
+        nthreads=num.threads, 
+        batch=batch$index
+    )
 
     rownames(output$statistics) <- x$rownames
     if (!is.null(batch$index)) {
